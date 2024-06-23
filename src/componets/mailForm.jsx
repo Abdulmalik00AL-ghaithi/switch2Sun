8d61a68faa653a032f58f1f57dcc8d79916d2fe6
Abdulmalik AL-Ghaithi <abdulmalik00alghaithi@gmail.com>
2024-06-23 19:52:31 +0300

    fixed some bugs in form

--- src/componets/mailForm.jsx
+++ src/componets/mailForm.jsx
@@ -1,25 +1,54 @@
 import React from "react";
 
 export default function MailForm(){
+    const [FormData,setFormData] = React.useState({
+        firstName:"",
+        lastName:"",
+        email:"",
+        comment:""
+    })
 
+    const handleChange = event =>{
+        setFormData(prev => { return{...prev,
+        [event.target.name]:[event.target.value]                                
+        }})
+    }
     return(
         <>
             <form>
                 <h2>Contact met ons</h2>
                 <label>
-                Naam<input type="text" />
+                Naam<input  type="text"
+                            name="firstName"
+                            onChange={handleChange} 
+                            value={FormData.firstName}
+                            />
                 </label>
 
                 <label>
-                Achter naam<input type="text" />
+                Achter naam
+                <input  type="text" 
+                        name="lastName"
+                        onChange={handleChange}
+                        value={FormData.lastName}
+                                />
                 </label>
 
                 <label>
-                Email*<input type="email" />
+                Email*
+                <input  type="email" 
+                        name="email"
+                        onChange={handleChange}
+                        value={FormData.email}
+                        />
                 </label>
 
                 <label>
-                Bericht*<textarea placeholder=""></textarea>
+                Bericht*<textarea name="comment"
+                                  onChange={handleChange}
+                                  value={FormData.comment}
+                                  >
+                        </textarea>
                 </label>
                 <button>Verstuur</button>
 
